c1064f2e7b4439ebe74b519c5855525cc33ce9f8
Kyle Kilby <kyle.lee.kilby@gmail.com>
2021-02-05 21:02:44 -0600

    node/express server skeleton setup

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/micro-life-farms', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error:'));
+db.once('open', () => {
+  console.log('Database Connected');
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000');
+});
